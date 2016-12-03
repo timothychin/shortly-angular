61feb1cbb58de32ec78057f7a3eb342d71f243c8
timothychin <timm.chin@gmail.com>
2016-12-02 19:16:59 -0800

    Implements shorten route and controller

--- client/app/app.js
+++ client/app/app.js
@@ -15,8 +15,11 @@ angular.module('shortly', [
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
     })
-    // Your code here
-
+    .when('/shorten', {
+      templateUrl: 'app/shorten/shorten.html',
+      controller: 'ShortenController',
+      authenticate: true
+    })
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
   $httpProvider.interceptors.push('AttachTokens');
